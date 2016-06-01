3d37b64a0e04beae9e4f3a0521e449f493d94da4
DONNU-5-Chebakov-Volodymyr <chebakov_v@inbox.ru>
2016-06-01 22:14:43 +0300

    add fancybox

--- Submission report/js/script.js
+++ Submission report/js/script.js
@@ -1,12 +1,16 @@
-  $(function() {
-    $( "#tabs" ).tabs();
-	  $(".btnPrev").on('click', function () {
-        $("#tabs").tabs("option", "active", $("#tabs").tabs('option', 'active') - 1);
-		  $("body").animate({"scrollTop":0},"slow");
-    });
-	  $(".btnNext").on('click', function () {
-        $("#tabs").tabs("option", "active", $("#tabs").tabs('option', 'active') + 1);
-		  $("body").animate({"scrollTop":0},"slow");
-    });
-	   
+  $(function () {
+  	$("#tabs").tabs();
+  	$(".btnPrev").on('click', function () {
+  		$("#tabs").tabs("option", "active", $("#tabs").tabs('option', 'active') - 1);
+  		$("body").animate({
+  			"scrollTop": 0
+  		}, "slow");
+  	});
+  	$(".btnNext").on('click', function () {
+  		$("#tabs").tabs("option", "active", $("#tabs").tabs('option', 'active') + 1);
+  		$("body").animate({
+  			"scrollTop": 0
+  		}, "slow");
+  	});
+  	$(".fancybox").fancybox();
   });
